a02c3fe4f5ab8c6adda5c7f3a9621dd93359e7d3
acaranta <arthur@caranta.com>
2021-02-26 06:20:17 +0100

    Added option to restart by image or container name and name check for special characters

--- docker-controller.py
+++ docker-controller.py
@@ -6,6 +6,7 @@ import yaml
 import glob
 import sys
 import os
+import re
 import subprocess
 
 from pprint import pprint
@@ -18,6 +19,9 @@ if os.getenv('REDIS_SRV') != None:
 yamlpath = "/appdata"
 if os.getenv('YAML_PATH') != None:
   yamlpath = os.getenv('YAML_PATH')
+
+def special_match(strg, search=re.compile(r'[^a-zA-Z0-9\-_:/]').search):
+  return not bool(search(strg))
 
 async def main():
   # Get messages from REDIS queue
@@ -66,6 +70,53 @@ async def main():
         print("Pruning Results : " + pruningresult)
         print("##################################################")
         send_status(redis, "Pruning done : " + pruningresult, "info")
+    if action == "restart-image":
+        checkimg = imgdata['imgfull'] 
+        if special_match(checkimg):
+            print("\n##################################################")
+            print(datetime.now())
+            print("Restart containers by image : " + checkimg)
+            print("--------------------------------------------------")
+            # Find if image is used in compose files, prepare cmd if so and run it
+            rescmd = ""
+            list_of_files = glob.glob(yamlpath + '/docker*.yml')           # create the list of file
+            for file_name in list_of_files:
+              with open(file_name, 'r') as file:
+                composeconfig = yaml.load(file, Loader=yaml.FullLoader)
+                for svc in composeconfig["services"]:
+                  if composeconfig["services"][svc]["image"] == checkimg or "library/" + composeconfig["services"][svc]["image"] == checkimg:
+                     send_status(redis, "Restarting " + checkimg + " in " + file_name, "info")
+                     rescmd = "cd " + yamlpath + " ; ./stack.sh " + file_name + " restart " + svc + " ; "
+                     print("Executing : " + rescmd)
+                     os.system(rescmd)
+            if rescmd:
+              print("Done restarting : " + checkimg)
+              print("##################################################")
+              send_status(redis, "Work done for " + checkimg, "info")
+            else:
+              print("Image " + checkimg + " not found in compose files")
+              print("##################################################")
+              send_status(redis, "Image " + checkimg + " not found in compose files", "info")
+        else:
+            print("##################################################")
+            print("Restart by image : image name contains forbidden characters")
+            print("##################################################")
+            send_status(redis, "Image restart failed, '"+checkimg+"' contains illegal characters ", "info")
+    if action == "restart-container":
+        container = imgdata['container'] 
+        if special_match(container):
+            rescmd = "docker restart " + container
+            print("##################################################")
+            print("Trying to restart " + container)
+            restartresult = subprocess.check_output(rescmd, shell=True).decode("utf-8")
+            print("Restart Results : " + restartresult)
+            print("##################################################")
+            send_status(redis, "Restart done : " + restartresult, "info")
+        else:
+            print("##################################################")
+            print("Container restart : container name contains forbidden characters")
+            print("##################################################")
+            send_status(redis, "Restart failed, '"+container+"' contains illegal characters ", "info")
 
 
 def send_status(redis, message, msgtype):
