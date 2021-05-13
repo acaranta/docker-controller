6782144eb05c522699d83096d21e6e844fd36ec8
acaranta <arthur@caranta.com>
2021-05-13 15:16:54 +0000

    Initial mqtt version of the controller

--- docker-controller.py
+++ docker-controller.py
@@ -1,38 +1,59 @@
 #!/usr/bin/python3
-import asyncio
-import aioredis
 import json 
 import yaml
 import glob
 import sys
 import os
 import re
+import time
 import subprocess
 import socket
+import paho.mqtt.client as mqtt
 
 from pprint import pprint
 from datetime import datetime
 
 #### OPTS ####
-redisserver = "localhost:6379"
-if os.getenv('REDIS_SRV') != None:
-  redisserver = os.getenv('REDIS_SRV')
+mqttServer = "192.168.10.12"
+if os.getenv('MQTT_SRV') != None:
+  mqttServer = os.getenv('MQTT_SRV')
+mqttPort = 1883
+if os.getenv('MQTT_PORT') != None:
+  mqttPort = os.getenv('MQTT_PORT')
 yamlpath = "/appdata"
 if os.getenv('YAML_PATH') != None:
   yamlpath = os.getenv('YAML_PATH')
 deamonName = socket.gethostname()
+if os.getenv('CTRL_HOST') != None:
+  deamonName = os.getenv('CTRL_HOST')
 
+mqttRootTopic = "dkr-ctrl"
+mqttTopics = [(mqttRootTopic + "/" + deamonName + "/action",1),(mqttRootTopic + "/ALL/action",1)]
 
 def special_match(strg, search=re.compile(r'[^a-zA-Z0-9\-_:/]').search):
   return not bool(search(strg))
 
-async def main():
-  # Get messages from REDIS queue
-  redis = await aioredis.create_redis('redis://'+redisserver+'/0', encoding='utf-8')
+def publish(mqttClient,route, payload, msgtype):
+    resp = {}
+    resp['message'] = payload
+    resp['type'] = msgtype
+    resp['host'] = deamonName
+    return mqttClient.publish(mqttRootTopic + "/" + deamonName + "/" + route, json.dumps(resp))
 
-  while True:
-    msg = await redis.blpop('docker-controller')
-    imgdata = json.loads(msg[1])
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to broker")
+        global Connected                #Use global variable
+        Connected = True                #Signal connection
+    else:
+        print("Connection failed")
+
+
+def on_message(client, userdata, message):
+    data = message.payload
+    receive=data.decode("utf-8")
+    imgdata = json.loads(receive)
+    print ("Message received: "  + str(imgdata))
 
     action = imgdata['action'] 
     if action == "imgupdate":
@@ -49,22 +70,22 @@ async def main():
             composeconfig = yaml.load(file, Loader=yaml.FullLoader)
             for svc in composeconfig["services"]:
               if composeconfig["services"][svc]["image"] == checkimg or "library/" + composeconfig["services"][svc]["image"] == checkimg:
-                 send_status(redis, "Pulling " + checkimg + " in " + file_name, "info")
+                 publish(client,"status", "Pulling " + checkimg + " in " + file_name, "info")
                  rescmd = "cd " + yamlpath + " ; ./stack.sh " + file_name + " pull " + svc + " ; "
                  print("Executing : " + rescmd)
                  os.system(rescmd)
-                 send_status(redis, "Updating " + checkimg + " in " + file_name, "info")
+                 publish(client,"status", "Updating " + checkimg + " in " + file_name, "info")
                  rescmd = "cd " + yamlpath + " ; ./stack.sh " + file_name + " up " + svc + " ; "
                  print("Executing : " + rescmd)
                  os.system(rescmd)
         if rescmd:
           print("Done for : " + checkimg)
           print("##################################################")
-          send_status(redis, "Work done for " + checkimg, "info")
+          publish(client,"status", "Work done for " + checkimg, "info")
         else:
           print("Image " + checkimg + " not found in compose files")
           print("##################################################")
-          send_status(redis, "Image " + checkimg + " not found in compose files", "info")
+          publish(client,"status", "Image " + checkimg + " not found in compose files", "info")
     elif action == "pruning":
         rescmd = "docker system prune --volumes -f"
         print("##################################################")
@@ -72,7 +93,7 @@ async def main():
         pruningresult = subprocess.check_output(rescmd, shell=True).decode("utf-8")
         print("Pruning Results : " + pruningresult)
         print("##################################################")
-        send_status(redis, "Pruning done : " + pruningresult, "info")
+        publish(client,"status", "Pruning done : " + pruningresult, "info")
     elif action == "restart-image":
         checkimg = imgdata['imgfull'] 
         if special_match(checkimg):
@@ -88,23 +109,23 @@ async def main():
                 composeconfig = yaml.load(file, Loader=yaml.FullLoader)
                 for svc in composeconfig["services"]:
                   if composeconfig["services"][svc]["image"] == checkimg or "library/" + composeconfig["services"][svc]["image"] == checkimg:
-                     send_status(redis, "Restarting " + checkimg + " in " + file_name, "info")
+                     publish(client,"status", "Restarting " + checkimg + " in " + file_name, "info")
                      rescmd = "cd " + yamlpath + " ; ./stack.sh " + file_name + " restart " + svc + " ; "
                      print("Executing : " + rescmd)
                      os.system(rescmd)
             if rescmd:
               print("Done restarting : " + checkimg)
               print("##################################################")
-              send_status(redis, "Work done for " + checkimg, "info")
+              publish(client,"status", "Work done for " + checkimg, "info")
             else:
               print("Image " + checkimg + " not found in compose files")
               print("##################################################")
-              send_status(redis, "Image " + checkimg + " not found in compose files", "info")
+              publish(client,"status", "Image " + checkimg + " not found in compose files", "info")
         else:
             print("##################################################")
             print("Restart by image : image name contains forbidden characters")
             print("##################################################")
-            send_status(redis, "Image restart failed, '"+checkimg+"' contains illegal characters ", "info")
+            publish(client,"status", "Image restart failed, '"+checkimg+"' contains illegal characters ", "info")
     elif action == "restart-container":
         container = imgdata['container'] 
         if special_match(container):
@@ -114,32 +135,44 @@ async def main():
             restartresult = subprocess.check_output(rescmd, shell=True).decode("utf-8")
             print("Restart Results : " + restartresult)
             print("##################################################")
-            send_status(redis, "Restart done : " + restartresult, "info")
+            publish(client,"status", "Restart done : " + restartresult, "info")
         else:
             print("##################################################")
             print("Container restart : container name contains forbidden characters")
             print("##################################################")
-            send_status(redis, "Restart failed, '"+container+"' contains illegal characters ", "info")
+            publish(client,"status", "Restart failed, '"+container+"' contains illegal characters ", "info")
     elif action == "container-list":
             rescmd = "docker ps --format='{{json .}}'"
             print("##################################################")
             restartresult = subprocess.check_output(rescmd, shell=True).decode("utf-8")
             print("Docker PS Done")
             print("##################################################")
-            send_status(redis, restartresult, "pslist")
+            publish(client,"pslist", restartresult, "pslist")
 
     else: 
         print("##################################################")
         print("Action unknown : "+action)
         print("##################################################")
-        send_status(redis, "Action unknown, '"+action+"'" , "info")
+        publish(client,"status", "Action unknown, '"+action+"'" , "info")
+    
 
-def send_status(redis, message, msgtype):
-    resp = {}
-    resp['message'] = message
-    resp['type'] = msgtype
-    return redis.rpush('docker-controller-resp', json.dumps(resp))
 
-print(str(datetime.now()) + " -- Starting Work Loop ...")
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+#Setup MQTT Connection
+Connected = False 
+client = mqtt.Client(deamonName)
+client.on_connect = on_connect   
+client.on_message = on_message
+client.connect(mqttServer, mqttPort)
+client.loop_start()
+while Connected != True:    #Wait for connection
+    time.sleep(0.1)
+client.subscribe(mqttTopics)
+
+try:
+  while True:
+    time.sleep(1)
+except KeyboardInterrupt:
+    print ("exiting")
+
+client.disconnect()
+client.loop_stop()
