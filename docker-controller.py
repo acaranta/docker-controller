16e85b2f53771996edffd4d613a1731361e3be6b
acaranta <arthur@caranta.com>
2025-01-24 10:43:06 +0100

    Refactor MQTT message handler to streamline container and image management actions, adding functions for stopping containers and restarting images with improved error handling and status notifications.

--- docker-controller.py
+++ docker-controller.py
@@ -124,7 +124,93 @@ def container_pruning(imgdata):
     print("Pruning Results : " + pruningresult)
     print("##################################################")
     publish(client,"status", "Pruning done : " + pruningresult, "info", imgdata)
-    
+
+def container_stop(imgdata):
+    container = imgdata['container'] 
+    pslist = get_container_list('json')
+    if container in pslist['names']:
+      if special_match(container):
+          rescmd = "docker stop " + container
+          print("##################################################")
+          print("Trying to stop " + container)
+          restartresult = subprocess.check_output(rescmd, shell=True).decode("utf-8")
+          print("Stop Results : " + restartresult)
+          print("##################################################")
+          publish(client,"status", "Stop done : " + restartresult, "info", imgdata)
+      else:
+          print("##################################################")
+          print("Container stop : container name contains forbidden characters")
+          print("##################################################")
+          publish(client,"status", "stop failed, '"+container+"' contains illegal characters ", "info", imgdata)
+    else:
+      print("##################################################")
+      print("Container stop : container not found on this host")
+      print("##################################################")
+
+def image_update(imgdata):
+    checkimg = imgdata['imgfull'] 
+    print("\n##################################################")
+    print(datetime.now())
+    print("Received hook call for " + checkimg)
+    print("--------------------------------------------------")
+    # Find if image is used in compose files, prepare cmd if so and run it
+    rescmd = ""
+    list_of_files = glob.glob(yamlpath + '/docker*.yml')           # create the list of file
+    for file_name in list_of_files:
+      with open(file_name, 'r') as file:
+        composeconfig = yaml.load(file, Loader=yaml.FullLoader)
+        for svc in composeconfig["services"]:
+          if 'image' in composeconfig["services"][svc]:
+              if composeconfig["services"][svc]["image"] == checkimg or "library/" + composeconfig["services"][svc]["image"] == checkimg:
+                  publish(client,"status", "Pulling " + checkimg + " in " + file_name, "info", imgdata)
+                  rescmd = "cd " + yamlpath + " ; ./stack.sh " + file_name + " pull " + svc + " ; "
+                  print("Executing : " + rescmd)
+                  os.system(rescmd)
+                  publish(client,"status", "Updating " + checkimg + " in " + file_name, "info", imgdata)
+                  rescmd = "cd " + yamlpath + " ; ./stack.sh " + file_name + " up " + svc + " ; "
+                  print("Executing : " + rescmd)
+                  os.system(rescmd)
+    if rescmd:
+      print("Done for : " + checkimg)
+      print("##################################################")
+      publish(client,"status", "Work done for " + checkimg, "info", imgdata)
+    else:
+      print("Image " + checkimg + " not found in compose files")
+      print("##################################################")
+      publish(client,"status", "Image " + checkimg + " not found in compose files", "info", imgdata)
+
+def image_restart(imgdata):
+    checkimg = imgdata['imgfull'] 
+    if special_match(checkimg):
+        print("\n##################################################")
+        print(datetime.now())
+        print("Restart containers by image : " + checkimg)
+        print("--------------------------------------------------")
+        # Find if image is used in compose files, prepare cmd if so and run it
+        rescmd = ""
+        list_of_files = glob.glob(yamlpath + '/docker*.yml')           # create the list of file
+        for file_name in list_of_files:
+          with open(file_name, 'r') as file:
+            composeconfig = yaml.load(file, Loader=yaml.FullLoader)
+            for svc in composeconfig["services"]:
+              if composeconfig["services"][svc]["image"] == checkimg or "library/" + composeconfig["services"][svc]["image"] == checkimg:
+                  publish(client,"status", "Restarting " + checkimg + " in " + file_name, "info", imgdata)
+                  rescmd = "cd " + yamlpath + " ; ./stack.sh " + file_name + " restart " + svc + " ; "
+                  print("Executing : " + rescmd)
+                  os.system(rescmd)
+        if rescmd:
+          print("Done restarting : " + checkimg)
+          print("##################################################")
+          publish(client,"status", "Work done for " + checkimg, "info", imgdata)
+        else:
+          print("Image " + checkimg + " not found in compose files")
+          print("##################################################")
+          publish(client,"status", "Image " + checkimg + " not found in compose files", "info", imgdata)
+    else:
+        print("##################################################")
+        print("Restart by image : image name contains forbidden characters")
+        print("##################################################")
+        publish(client,"status", "Image restart failed, '"+checkimg+"' contains illegal characters ", "info", imgdata)  
 ### MQTT Message Handler ###
 def on_message(client, userdata, message):
     data = message.payload
@@ -134,91 +220,17 @@ def on_message(client, userdata, message):
 
     action = imgdata['action'] 
     if action == "imgupdate":
-        checkimg = imgdata['imgfull'] 
-        print("\n##################################################")
-        print(datetime.now())
-        print("Received hook call for " + checkimg)
-        print("--------------------------------------------------")
-        # Find if image is used in compose files, prepare cmd if so and run it
-        rescmd = ""
-        list_of_files = glob.glob(yamlpath + '/docker*.yml')           # create the list of file
-        for file_name in list_of_files:
-          with open(file_name, 'r') as file:
-            composeconfig = yaml.load(file, Loader=yaml.FullLoader)
-            for svc in composeconfig["services"]:
-              if 'image' in composeconfig["services"][svc]:
-                  if composeconfig["services"][svc]["image"] == checkimg or "library/" + composeconfig["services"][svc]["image"] == checkimg:
-                      publish(client,"status", "Pulling " + checkimg + " in " + file_name, "info", imgdata)
-                      rescmd = "cd " + yamlpath + " ; ./stack.sh " + file_name + " pull " + svc + " ; "
-                      print("Executing : " + rescmd)
-                      os.system(rescmd)
-                      publish(client,"status", "Updating " + checkimg + " in " + file_name, "info", imgdata)
-                      rescmd = "cd " + yamlpath + " ; ./stack.sh " + file_name + " up " + svc + " ; "
-                      print("Executing : " + rescmd)
-                      os.system(rescmd)
-        if rescmd:
-          print("Done for : " + checkimg)
-          print("##################################################")
-          publish(client,"status", "Work done for " + checkimg, "info", imgdata)
-        else:
-          print("Image " + checkimg + " not found in compose files")
-          print("##################################################")
-          publish(client,"status", "Image " + checkimg + " not found in compose files", "info", imgdata)
+        image_update(imgdata)    
     elif action == "pruning":
         container_pruning(imgdata)
-        
     elif action == "restart-image":
-        checkimg = imgdata['imgfull'] 
-        if special_match(checkimg):
-            print("\n##################################################")
-            print(datetime.now())
-            print("Restart containers by image : " + checkimg)
-            print("--------------------------------------------------")
-            # Find if image is used in compose files, prepare cmd if so and run it
-            rescmd = ""
-            list_of_files = glob.glob(yamlpath + '/docker*.yml')           # create the list of file
-            for file_name in list_of_files:
-              with open(file_name, 'r') as file:
-                composeconfig = yaml.load(file, Loader=yaml.FullLoader)
-                for svc in composeconfig["services"]:
-                  if composeconfig["services"][svc]["image"] == checkimg or "library/" + composeconfig["services"][svc]["image"] == checkimg:
-                      publish(client,"status", "Restarting " + checkimg + " in " + file_name, "info", imgdata)
-                      rescmd = "cd " + yamlpath + " ; ./stack.sh " + file_name + " restart " + svc + " ; "
-                      print("Executing : " + rescmd)
-                      os.system(rescmd)
-            if rescmd:
-              print("Done restarting : " + checkimg)
-              print("##################################################")
-              publish(client,"status", "Work done for " + checkimg, "info", imgdata)
-            else:
-              print("Image " + checkimg + " not found in compose files")
-              print("##################################################")
-              publish(client,"status", "Image " + checkimg + " not found in compose files", "info", imgdata)
-        else:
-            print("##################################################")
-            print("Restart by image : image name contains forbidden characters")
-            print("##################################################")
-            publish(client,"status", "Image restart failed, '"+checkimg+"' contains illegal characters ", "info", imgdata)
+        image_restart(imgdata)  
     elif action == "restart-container":
-            container_restart(imgdata)
+        container_restart(imgdata)
     elif action == "stop-container":
-        container = imgdata['container'] 
-        if special_match(container):
-            rescmd = "docker stop " + container
-            print("##################################################")
-            print("Trying to stop " + container)
-            restartresult = subprocess.check_output(rescmd, shell=True).decode("utf-8")
-            print("Stop Results : " + restartresult)
-            print("##################################################")
-            publish(client,"status", "Stop done : " + restartresult, "info", imgdata)
-        else:
-            print("##################################################")
-            print("Container stop : container name contains forbidden characters")
-            print("##################################################")
-            publish(client,"status", "stop failed, '"+container+"' contains illegal characters ", "info", imgdata)
+        container_stop(imgdata)
     elif action == "container-list":
         container_list(imgdata)
-
     else: 
         print("##################################################")
         print("Action unknown : "+action)
